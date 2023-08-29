b2eb693ee06826e33f7b726cb952a422de144f72
unknown <leosantana.silva27@gmail.com>
2023-08-29 13:24:42 -0300

    adding responsive layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,41 @@
-import Link from "next/link";
+'use client'
 
+import Link from "next/link"
+import { Menu } from 'lucide-react'
+import { useState } from 'react'
 export default function Header() {
+
+    const [menuShow, setMenuShow] = useState(false)
+
+    const openCloseMenu = () => {
+        setMenuShow(!menuShow)
+    }
+
     return (
-        <header className="w-full h-20 bg-[var(--green)] ">
-            <nav className="w-full h-full flex items-center justify-around">
-                <div className="flex items-center gap-4">
-                    <img src="/logo.png" alt="logo"
-                    className="w-[90px]" />
-                    <span className="text-2xl font-bold">Desperdicio Zero</span>
-                </div>
-                <ul className="flex items-center gap-6 font-medium text-lg">
-                    <li>
+        <header className="w-full h-20 bg-[var(--green)]">
+            <nav className="w-full h-full flex items-center justify-around relative z-20">
+               <Link href="/">
+                <div className="flex items-center gap-4 md:gap-2">
+                        <img src="/logo.png" alt="logo"
+                        className="w-[90px]" />
+                        <span className="text-2xl font-bold whitespace-nowrap">Desperdício Zero</span>
+                    </div>
+               </Link>
+                <ul className={`flex items-center gap-6 font-medium text-lg md:absolute md:flex-col md:items-start md:bg-[#FBEEDB] md:px-6 md:py-4 md:rounded md:right-0 md:transition-top md:duration-500 md:z-0 ${menuShow ? 'md:visible md:top-20' : 'md:invisible md:-top-full'}`}>
+                    <li className="hover:text-zinc-800 transition">
                         <Link href="/cardapio">Cardapio</Link>
                     </li>
-                    <li>
+                    <li className="hover:text-zinc-800 transition">
                         <Link href="/Receitas">Receitas</Link>
                     </li>
-                    <li>
+                    <li className="hover:text-zinc-800 transition">
                         <Link href="/Dahsboard">Dashboard</Link>
                     </li>
                 </ul>
+                <button onClick={openCloseMenu}
+                className="hidden md:block cursor-pointer z-10">
+                    <Menu size={36}/>
+                </button>
             </nav>
         </header>
     )
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
     }, 4000)
 
     return (
-        <section className={`flex items-center justify-center w-full h-screen fixed top-0 left-0 z-50 bg-[#FFE4C5] transition duration-700 ${!introShow && 'opacity-0'}`}>
+        <section className={`flex items-center justify-center w-full h-screen fixed top-0 left-0 z-50 bg-[#FFE4C5] transition duration-700 ${!introShow && 'opacity-0 hidden'}`}>
             <div>
                 <video autoPlay muted
                 className="w-full max-w-4xl">
