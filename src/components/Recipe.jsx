649589f55b007b8cfb249aa474bd6c741b9c7276
Lzinnxl007 <97236409+Lzinnxl007@users.noreply.github.com>
2023-09-08 14:12:08 -0300

    correting import error

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -5,7 +5,7 @@ import { useSearchParams } from "next/navigation"
 import  { useEffect, useState } from 'react'
 import { useTheme } from 'next-themes'
 import { Suspense } from 'react'
-import Loading from '/loading'
+import Loading from './loading'
 export default function Recipe() {
 
     const { theme } = useTheme()
