d18eb1d103123cff8c48885e82eeb0fe3a609667
Lzinnxl007 <97236409+Lzinnxl007@users.noreply.github.com>
2023-09-09 13:27:24 -0300

    add spin loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -7,13 +7,13 @@ export default function Loading() {
 
     setTimeout(() => {
         setShow(false)
-    }, 10000)
+    }, 2000)
 
     return (
         <div>
             {show && (
-                <div className="flex w-screen h-screen z-50 items-center justify-center text-6xl absolute top-0 left-0 bg-red-300">
-                    Carregando...
+                <div className="flex w-screen h-screen z-50 items-center justify-center text-6xl absolute top-0 left-0 bg-zinc-50 bg-opacity-60 backdrop-blur-lg">
+                    <div className="w-[40vw] aspect-square rounded-full border-solid border-8 border-t-[var(--green)] animation-spin bg-transparent" />
                 </div>
             )}
         </div>
