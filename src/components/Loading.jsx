f631a022bc54788c49f4cefa683b2a2ecce0fb6e
Lzinnxl007 <97236409+Lzinnxl007@users.noreply.github.com>
2023-09-09 13:51:41 -0300

    ...

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -21,7 +21,7 @@ export default function Loading() {
         <div>
             {show && (
                 <div className="flex w-screen h-screen z-50 flex items-center justify-center text-6xl absolute top-0 left-0 bg-zinc-50 bg-opacity-60 backdrop-blur-lg">
-                    <div className="w-[200px] aspect-square rounded-full border-solid border-16 border-t-orange-400 animate-spin" />
+                    <div className="w-48 aspect-square rounded-full border-solid border-16 border-t-orange-400 animate-spin" />
                 </div>
             )}
         </div>
