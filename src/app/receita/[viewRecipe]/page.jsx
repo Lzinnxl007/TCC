76475ddb58e82dd00984ffd7f08500f50cbcbd0e
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-09-12 11:29:32 -0300

    search id query

--- src/app/receita/[viewRecipe]/page.jsx
+++ src/app/receita/[viewRecipe]/page.jsx
@@ -4,11 +4,16 @@ import Recipe from "@/components/Recipe"
 import Header from "@/components/Header"
 import Share from "@/components/Share"
 import { usePathname } from "next/navigation"
+import { useSearchParams } from "next/navigation"
 export default function ViewRecipe() {
 
    const path = usePathname()
+   const searchParams = useSearchParams()
 
-   const query = path.split('/receita/')[1]
+   const id = searchParams.get('id')
+
+   const query = `${path.split('/receita/')[1]}id=${id}`
+
 
     return (
         <section>
