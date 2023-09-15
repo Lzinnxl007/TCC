9cf2e6af7c8d1b3803694b13839194bb6ad8b072
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-09-14 23:50:36 -0300

    tests

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,24 +5,29 @@ import BeatLoader from "react-spinners/BeatLoader"
 export default function Loading() {
 
     const [show, setShow] = useState(true)
+    const [scroll, setScroll] = useState(0)
 
     setTimeout(() => {
         setShow(false)
     }, 1000)
 
     useEffect(() => {
+        setScroll(window.scrollY)
         if(show) {
-            window.document.body.style.overflow = 'hidden'
+            window.document.body.style.overflow = 'hidden' 
         } else {
+            window.scrollTo(0, 0)
             window.document.body.style.overflow = 'scroll'
         }
     }, [show])
+
+    
 
 
     return (
         <div>
             {show && (
-                <div className="w-screen h-screen z-50 flex items-center justify-center text-6xl absolute top-0 left-0 bg-zinc-50 bg-opacity-60 backdrop-blur-lg">
+                <div className={`w-screen h-screen z-50 flex items-center justify-center text-6xl absolute top-[${scroll}px] left-0 bg-zinc-50 bg-opacity-60 backdrop-blur-lg`}>
                     <BeatLoader
                     color="#A3BD36"
                     loading={true}
@@ -34,4 +39,3 @@ export default function Loading() {
         </div>
     )
 }
-//bg-opacity-60 backdrop-blur-lg
--- src/utils/GetRecipes.js
+++ src/utils/GetRecipes.js
@@ -2,8 +2,6 @@ export const GetRecipes = async() => {
   const response = await fetch('https://desperdicio-zero-api.vercel.app/api/recipes')
 
     const data = await response.json()
-
-    console.log(data)
     
     return data
   }
