f76ed1950ada4b4ead1209a3db4efb9f2d508bc7
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-09-30 15:51:42 -0300

    correting prisma migrations

--- prisma/migrations/20230930185111_desperdicio_zero/migration.sql
+++ prisma/migrations/20230930185111_desperdicio_zero/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Comments` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Comments";
-
 -- CreateTable
 CREATE TABLE "comments" (
     "id" SERIAL NOT NULL,
@@ -26,8 +17,22 @@ CREATE TABLE "emails" (
     CONSTRAINT "emails_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "photo" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "comments_id_key" ON "comments"("id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "emails_id_key" ON "emails"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
