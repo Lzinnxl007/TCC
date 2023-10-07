ec9a00246c775605e50f1fbdff63f941c5f24e4f
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-10-06 23:06:28 -0300

    add next-seo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+
 
 import Footer from "@/components/Default/Footer"
 import Header from "@/components/Default/Header"
@@ -7,18 +7,35 @@ import Intro from "@/components/Initial/Intro"
 import Main from "@/components/Initial/Main"
 import Modal from "@/components/Initial/Modal"
 import VideoApresentation from "@/components/Initial/VideoApresentation"
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
 
   return (
     <div className="min-h-screen bg-[var(--orange)] overflow-hidden">
-        <Header/>
-        <Main/>
-        <VideoApresentation/>
-        <Footer/>
-        <ScrollToTop/>
-        <Modal/>
-        
+      <NextSeo
+        title="Desperdício Zero"
+        description="Cuide da sua saúde e do planeta!"
+        openGraph={{
+          title: 'Desperdício Zero',
+          description: 'Cuide da sua saúde e do planeta!',
+          images: [
+            {
+              url: '/next-seo-image.png',
+              width: 1200,
+              height: 630,
+              alt: 'Desperdício Zero Imagem',
+            },
+          ],
+        }}>
+          <Header/>
+          <Main/>
+          <VideoApresentation/>
+          <Footer/>
+          <ScrollToTop/>
+          <Modal/>
+      </NextSeo>
+       
     </div>
   )
 }
