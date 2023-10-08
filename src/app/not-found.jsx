c81b6f7eabe8d48bba71712289d46161e43f8186
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-10-08 19:31:46 -0300

    correting again

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Footer from "@/components/Default/Footer"
 import Header from "@/components/Default/Header"
 import Link from "next/link"
 
 export default function NotFound() {
+
+    const backToInitialPage = () => {
+        window.location.href = "/"
+    }
+    
     return (
         <section className="w-screen min-h-screen bg-[var(--light-grey)]">
             <Header/>
@@ -14,11 +21,11 @@ export default function NotFound() {
                     <p className="max-w-md lg:text-center lg:w-full lg:px-4">
                     Desculpe, a página que você está procurando não foi encontrada. Parece que você se perdeu na internet. Por favor, volte à página inicial para continuar sua jornada.
                     </p>
-                   <Link href="https://desperdiciozero.vercel.app">
-                        <button className="px-4 py-1.5 rounded bg-[var(--green)] text-zinc-50 font-medium">
-                            Voltar ao Início
-                        </button>
-                    </Link>
+                    <button onClick={backToInitialPage}
+                    className="px-4 py-1.5 rounded bg-[var(--green)] text-zinc-50 font-medium w-fit">
+                        Voltar ao Início
+                    </button>
+
                 </div>
                 <div className="max-w-lg lg:max-w-md md:max-w-sm">
                     <img src="/404-image.png" alt="404-image.png" 
