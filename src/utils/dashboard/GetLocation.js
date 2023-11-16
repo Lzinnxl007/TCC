c636f0a8c21f1e557039a2de4eaac952fa310604
Lzinnxl007 <leosantana.silva27@gmail.com>
2023-11-15 21:12:37 -0300

    changing apiKey

--- src/utils/dashboard/GetLocation.js
+++ src/utils/dashboard/GetLocation.js
@@ -2,14 +2,14 @@
 
 export const getLocationFromIP = async () => {
   try {
-    const response = await fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCj-etuTDqYsHM3lpUyUY6MBunz8goWjg8`, {
+    const response = await fetch(`https://www.googleapis.com/geolocation/v1/geolocate?key=${process.env.GOOGLE_CLOUD_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({ considerIp: true }), 
     })
-    
+
     if (!response.ok) {
       throw new Error('Erro ao obter a localização')
     }
